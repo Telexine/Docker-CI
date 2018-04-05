1db48199e9e64aa00599bd40e656db1409d72f41
Lexine Langford <tanuki360@hotmail.com>
2018-04-05 17:56:15 +0700

    Base Init

---
+++ app.js
@@ -0,0 +1,41 @@
+
+var express = require('express');
+var config = require('./service/config');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var http = require('http');
+var fs = require('fs');
+
+var pug = require('pug');
+/**\
+ *
+ *  DIRECTIVE 
+ *    var u = {UID: 5, userEmail: "rtester@testing.com", userPassword: "bd4eb56b41fc3663dfe2761ff34621a544ecfe27", userLastLogin: "2017-11-20T22:18:13.000Z", userToken: "cae45ae7e68ef8024d4ad5b56c68f263"}
+  res.render('index', { user: u });
+
+ */
+//const compiledFunction = pug.compileFile('./layouts/index.pug');
+app.set('view engine', 'pug')
+app.set("views", path.join(__dirname, "layouts"));
+
+
+//express
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+ 
+
+app.use('/public',express.static('public'));
+app.use('/node_modules',express.static('node_modules'));
+
+app.get("/", function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+
+})
+
+
+http.createServer(app).listen(3333, function () {
+    console.log('Server is Runing on localhost:3333');
+})
---
+++ service/config.js
@@ -0,0 +1,20 @@
+var  MongoClient  = require('mongodb');
+var Promise  = require('bluebird');
+var  logger  = require('winston');
+ 
+
+
+
+
+ var MongoClient  = function() {   MongoClient.connect(config.database.url, { promiseLibrary: Promise }, (err, db) => {
+        if (err) {
+          logger.warn(`Failed to connect to the database. ${err.stack}`);
+        }
+        app.locals.db = db;
+        app.listen(config.port, () => {
+          logger.info(`Node.js app is listening at http://localhost:${config.port}`);
+        });
+      });
+    
+};
+exports.conn = MongoClient;
